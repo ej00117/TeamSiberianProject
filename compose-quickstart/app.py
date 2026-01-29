0d36913622bbbafaea0f6787a87ba90b4fad5526
ZSeals <zs00058@my.westga.edu>
2026-01-29 14:09:28 -0500

    Compose Files

---
+++ compose-quickstart/app.py
@@ -0,0 +1,23 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError:
+            if retries == 0:
+                raise
+            retries -= 1
+            time.sleep(1)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n'
